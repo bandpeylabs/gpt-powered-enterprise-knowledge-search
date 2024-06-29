f8a9f6a3f88a9d2048757f57638f4b110ccbea28
RoozbehBandpey <r.bandpey67@gmail.com>
2024-06-30 00:13:49 +0200

    working on permission

--- assistant-demo/main.py
+++ assistant-demo/main.py
@@ -1,59 +1,46 @@
-from langchain.llms import OpenAI
-from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain, SequentialChain
-from dotenv import load_dotenv
-import argparse
-
-load_dotenv()
+import os
+from azure.identity import DefaultAzureCredential, get_bearer_token_provider
+from openai import AzureOpenAI
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--task", default="sum two numbers")
-parser.add_argument("--language", default="python")
-args = parser.parse_args()
+token_provider = get_bearer_token_provider(
+    DefaultAzureCredential(), "https://cognitiveservices.azure.com/.default")
 
-
-# No need to fetch this becasue the OPENAI_API_KEY is known by OpenAI
-# api_key = os.getenv('OPENAI_API_KEY')
-
-llm = OpenAI(
-    # openai_api_key=api_key
+client = AzureOpenAI(
+    azure_ad_token_provider=token_provider,
+    azure_endpoint=os.environ["AZURE_OPENAI_ENDPOINT"],
+    api_version="2024-05-01-preview",
 )
 
-code_prompt = PromptTemplate(
-    template="Write a short {language} code that will {task}",
-    input_variables=["language", "task"]
+# Create an assistant
+assistant = client.beta.assistants.create(
+    name="Math Assist",
+    instructions="You are an AI assistant that can write code to help answer math questions.",
+    tools=[{"type": "code_interpreter"}],
+    # You must replace this value with the deployment name for your model.
+    model="gpt-4-1106-preview"
 )
 
-error_checking_prompt = PromptTemplate(
-    template="Write a unit test for this code in {language}:\n{code}",
-    input_variables=["code", "language"]
-)
+print(assistant)
+# # Create a thread
+# thread = client.beta.threads.create()
 
-code_chain = LLMChain(
-    llm=llm,
-    prompt=code_prompt,
-    output_key="code"
-)
+# # Add a user question to the thread
+# message = client.beta.threads.messages.create(
+#     thread_id=thread.id,
+#     role="user",
+#     content="I need to solve the equation `3x + 11 = 14`. Can you help me?"
+# )
 
-error_checking_chain = LLMChain(
-    llm=llm,
-    prompt=error_checking_prompt,
-    output_key="unittest"
-)
+# # Run the thread and poll for the result
+# run = client.beta.threads.runs.create_and_poll(
+#     thread_id=thread.id,
+#     assistant_id=assistant.id,
+#     instructions="Please address the user as Jane Doe. The user has a premium account.",
+# )
 
-chain = SequentialChain(
-    chains=[code_chain, error_checking_chain],
-    input_variables=["task", "language"],
-    output_variables=["unittest", "code"]
-)
+# print("Run completed with status: " + run.status)
 
-result = chain({
-    "language": args.language,
-    "task": args.task
-})
-
-print("--------- code ---------")
-print(result["code"])
-print("--------- unittest ---------")
-print(result["unittest"])
+# if run.status == "completed":
+#     messages = client.beta.threads.messages.list(thread_id=thread.id)
+#     print(messages.to_json(indent=2))
