2169e97fba574d40507355a37ce1dc69b2e7927a
RoozbehBandpey <r.bandpey67@gmail.com>
2024-06-30 00:32:35 +0200

    assistant demo created

--- assistant-demo/main.py
+++ assistant-demo/main.py
@@ -14,33 +14,34 @@ client = AzureOpenAI(
 
 # Create an assistant
 assistant = client.beta.assistants.create(
-    name="Math Assist",
-    instructions="You are an AI assistant that can write code to help answer math questions.",
-    tools=[{"type": "code_interpreter"}],
+    name="sarcastic-chat",
+    instructions="You are a useless AI assistant. No matter what was the question, answer sarcastically and make fun of user!",
+    # tools=[{"type": "code_interpreter"}],
     # You must replace this value with the deployment name for your model.
-    model="gpt-4-1106-preview"
+    model="depl-assistant-dev"
 )
 
-print(assistant)
-# # Create a thread
-# thread = client.beta.threads.create()
+# Create a thread
+thread = client.beta.threads.create()
 
-# # Add a user question to the thread
-# message = client.beta.threads.messages.create(
-#     thread_id=thread.id,
-#     role="user",
-#     content="I need to solve the equation `3x + 11 = 14`. Can you help me?"
-# )
+# Add a user question to the thread
+message = client.beta.threads.messages.create(
+    thread_id=thread.id,
+    role="user",
+    content="I need to solve the equation `3x + 11 = 14`. Can you help me?"
+)
 
-# # Run the thread and poll for the result
-# run = client.beta.threads.runs.create_and_poll(
-#     thread_id=thread.id,
-#     assistant_id=assistant.id,
-#     instructions="Please address the user as Jane Doe. The user has a premium account.",
-# )
 
-# print("Run completed with status: " + run.status)
+# Run the thread and poll for the result
+run = client.beta.threads.runs.create_and_poll(
+    thread_id=thread.id,
+    assistant_id=assistant.id,
+    # Way to overwrite system instructions
+    # instructions="Please address the user as Roozbeh Bandpey. The user has a premium account.",
+)
 
-# if run.status == "completed":
-#     messages = client.beta.threads.messages.list(thread_id=thread.id)
-#     print(messages.to_json(indent=2))
+print("Run completed with status: " + run.status)
+
+if run.status == "completed":
+    messages = client.beta.threads.messages.list(thread_id=thread.id)
+    print(messages.to_json(indent=2))
